d47f0460111f5a08d5b5d9d875499e12e4d27e3d
labarruffe <lucas_barruffe@hotmail.com>
2019-09-15 23:59:01 -0300

    Create savings account class

---
+++ SavingsAccount.cs
@@ -0,0 +1,38 @@
+namespace worldBank
+{
+    public class SavingsAccount : Account, IDepositable
+    {
+        public SavingsAccount()
+        {
+            // All savings accounts should have the same Withdrawal Fee
+            this.WithdrawalFee = 4.99;
+        }
+
+        public override void Withdraw(double value)
+        {
+            if (value > this.Balance)
+            {
+                Console.WriteLine("This value exceeds your balance!");
+            } 
+            else 
+            {
+                this.Balance -= value;
+            }
+        }
+
+        public override double CheckBalance()
+        {
+            return this.Balance;
+        }
+        public override void Transfer(Account account, double value)
+        {
+            this.Withdraw(value);
+            account.Balance += value;   
+        }
+
+        public void Deposit(double value)
+        {
+            this.Balance += value;
+        }
+    }
+}
